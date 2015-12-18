759651cc87ad695714930356cb98347d9bb37515
sriharsha <sriharsha@myedutor.com>
2015-12-18 17:53:20 +0530

    Allows match method in routes for Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.append do
-  match "/gateway/:order_id/ccavenue/:payment_method_id" => 'ccavenue/gateway#show', :as => :gateway_ccavenue
-  match "/gateway/ccavenue/:id/callback" => 'ccavenue/gateway#callback', :as => :gateway_ccavenue_callback
+  match "/gateway/:order_id/ccavenue/:payment_method_id" => 'ccavenue/gateway#show', :as => :gateway_ccavenue , via: :all
+  match "/gateway/ccavenue/:id/callback" => 'ccavenue/gateway#callback', :as => :gateway_ccavenue_callback , via: :all
 end
